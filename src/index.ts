374e20cb54d75827e509d6c4dc9fe7958fdda9a2
robsorb <rob.schellingerhout@xs4all.nl>
2021-05-04 12:39:04 +0200

    use fetch to read the file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { Email, getCorrespondants, parseData } from "./data"
-import { div, loadFile, text } from "./utils"
+import { getCorrespondants, parseData } from "./data"
+import { div, text } from "./utils"
 
 
 window.addEventListener("load", () => {
     document.body.appendChild(div({}, [text('Hello World ❤️')]))
 
-    loadFile('enron-v1.csv', text => {
+    fetch('./enron-v1.csv')
+    .then(r => r.text())
+    .then(text => {
         let list = parseData(text)
         console.log(getCorrespondants(list))
         console.log(list.slice(0,10))
