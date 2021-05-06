123bf4755519211b7163854865c48ef44d8184b3
robsorb <rob.schellingerhout@xs4all.nl>
2021-05-06 11:02:34 +0200

    made event handler in load async

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,11 @@ import { getCorrespondants, parseData } from "./data"
 import { div, text } from "./utils"
 
 
-window.addEventListener("load", () => {
-    document.body.appendChild(div({}, [text('Hello World ❤️')]));
+window.addEventListener("load", async () => {
+    document.body.appendChild(div({}, [text('Hello World ❤️')]))
 
-    (async () => {
-        let file = await fetch('./enron-v1.csv')
-        let text = await file.text()
-        let list = parseData(text)
-        console.log(getCorrespondants(list))
-        console.log(list.slice(0,10))
-    })()
+    let file = await fetch('./enron-v1.csv')
+    let list = parseData(await file.text())
+    console.log(getCorrespondants(list))
+    console.log(list.slice(0,10))
 })
