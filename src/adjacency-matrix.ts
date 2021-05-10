4d85acae536b9174d396fd3de3bcb6f746768516
DukeBas <35384117+DukeBas@users.noreply.github.com>
2021-05-10 14:42:24 +0200

    Added filterCorrespondants

--- src/adjacency-matrix.ts
+++ src/adjacency-matrix.ts
@@ -6,41 +6,24 @@ const dataFile = require('../resources/static/enron-v1.csv');
 window.addEventListener("load", async () => {
     document.body.appendChild(div({}, [text('Adjacency-matrix')]))
 
+    // Get data
     let file = await fetch(dataFile.default);
     let emails = parseData(await file.text());
     const correspondants = getCorrespondants(emails); //dictionary with persons
-
-    const correspondantList: Person[] = []; // array with persons
-
-    // for (const item in correspondants) {
-    //     let person: Person;
-    //     let pseudoPerson = correspondants[item];
-        
-    //     // Object.assign(person, pseudoPerson);
-    //     person.emailAdress = pseudoPerson.emailAdress;
-    //     person.id = pseudoPerson.id;
-    //     person.title = pseudoPerson.title;
-        
-    //     correspondantList.push(person);        
-    // }
-
-    // console.log(correspondantList)
-
-    // let personInstance: {[id: number]: Person} = {}
-
-    // input ; lijstje job titles  --> Array
-    // output ; -correspondents met die job titles
-
-    filterCorrespondants(["CEO"], correspondantList)
     
+    //Creating array with person objects...
+    let correspondantList = Object.values(correspondants);
 
+
+    //Testing the function
+
+    let filtered = filterCorrespondants(["Unknown"], correspondantList);
+    console.log(filtered)
 });
 
-// Should return an array (filtered) with the persons who have one of the jobtitles that is given as an array (jobTitleList) in the input.
-//ALso requires an original array with persons as input (correspondants)
-//Still in progress
+//Returns an array (filtered) with the persons who have one of the jobtitles that is given as an array (jobTitleList) in the input.
 function filterCorrespondants(jobTitleList: Title[], correspondants: Person[]) {
-    let filtered: Person[] = []; // create an array for keeping the entries we want
+    let filtered: Person[] = [];
     for (let person in correspondants) {
         for (let job in jobTitleList){
             if (jobTitleList[job]===correspondants[person].title){
