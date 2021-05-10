f6948328140be74ef70699a0e885805c4dfa9156
robsorb <rob.schellingerhout@xs4all.nl>
2021-05-10 19:18:31 +0200

    Simple Vis.js demo
    Co-authored-by: jessefitie <jessefitie@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,48 @@ import { div, text } from "./utils"
 
 const dataFile = require('../resources/static/enron-v1.csv')
 
+import * as vis from "vis"
+
+import "vis/dist/vis.min.css"
 
 window.addEventListener("load", async () => {
-    document.body.appendChild(div({}, [text('Hello World ❤️')]))
 
-    let file = await fetch(dataFile.default)
-    let emails = parseData(await file.text())
+    const emails = parseData(await (await fetch(dataFile.default)).text()).slice(0, 100)
     const correspondants = getCorrespondants(emails)
-    console.table(correspondants)
-    console.table(emails.slice(0, 10))
+
+    const maxSent = Math.max(...emails.map(i => i.sentiment))
+    const minSent = Math.min(...emails.map(i => i.sentiment))
+
+    let nodes = new vis.DataSet(
+        Object.entries(correspondants)
+        .map(([k, i]) => {
+            return {
+                id: +k,
+                title: `${i.email}, ${i.title}`,
+                group: i.title
+            }
+        })
+    )
+    let edges = new vis.DataSet(
+        emails.map(i => {
+            return {
+                from: i.fromId, 
+                to: i.toId,
+                title: ""+i.sentiment,
+                width: (i.sentiment - minSent) / (maxSent - minSent) * 5
+            }   
+        })
+    )
+
+    const config = {
+        nodes: {
+            shape: 'dot',
+            size: 20,
+        },
+        edges: {
+            arrows: "to"
+        }
+    }
+
+    new vis.Network(document.body, {nodes: nodes, edges: edges}, config)
 })
