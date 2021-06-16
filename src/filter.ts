e0f3c2f71cebb70e5e19ef76c93d795d88bdcdef
robsorb <rob.schellingerhout@xs4all.nl>
2021-06-16 22:50:44 +0200

    A bunch more comments and cleanup

--- src/filter.ts
+++ src/filter.ts
@@ -10,21 +10,26 @@ import { arrayToObject, div, newElm, span, text } from "./utils"
 
 type FilterFunction = (email: Email, people: DataSet<Person>, emails: DataSet<Email>) => boolean
 
+/**
+ * This class creates and manages the HTML elements and events for the filter options for the visualisation
+ */
 export class FilterOptions {
 
     private container: HTMLElement
-    private menu: HTMLSelectElement
-    private textArea: HTMLTextAreaElement
-    private text: HTMLElement
-    private textWrapper: HTMLElement
+    private presetMenu: HTMLSelectElement
     private applyButton: HTMLElement
+
+    private functionBodyTextArea: HTMLTextAreaElement
+    private highlightedFunctionBodyText: HTMLElement
+    private functionWrapper: HTMLElement
+    private functionSignature: HTMLElement
     private errorElm: HTMLElement
 
     private presets: {[val:string]: [string, string]} = {
-        none: ["None", "true"],
-        sent: ["Sentiment", "Math.abs(email.sentiment) > 0.069"],
-        title: ["Title", "email.fromJobtitle == 'CEO' || email.toJobtitle == 'CEO'"],
-        type: ["Type", "email.messageType == 'TO'"],
+        none:  ["None",        "true"                                                      ],
+        sent:  ["Sentiment",   "Math.abs(email.sentiment) > 0.069"                         ],
+        title: ["Title",       "email.fromJobtitle == 'CEO' || email.toJobtitle == 'CEO'"  ],
+        type:  ["Type",        "email.messageType == 'TO'"                                 ],
     }
 
 
@@ -33,25 +38,61 @@ export class FilterOptions {
     constructor(container: HTMLElement) {
         this.container = container
 
-        this.menu = newElm("select", {}, 
+        this.createElements()
+        this.setupEvents()
+        this.clearErrorMessage()
+        this.updateText()
+        highlightElement(this.functionSignature)
+    }
+
+    public filterEmails(emails: Observable<Email[]>): Observable<Email[]> {
+        return new Observable<Email[]>(sub => {
+            combineLatest([
+                emails,
+                this.filterFunction
+            ]).subscribe(([emails, filterFunc]) => {
+                const emailMap = arrayToObject(emails, email => email.id)
+                const people = getCorrespondants(emails)
+    
+                try {
+                    sub.next(emails.filter(email => filterFunc(email, people, emailMap)))
+                    this.clearErrorMessage()
+                } catch (e) {
+                    this.setErrorMessage(e.message)
+                }
+            })
+        }).pipe(share())
+    }
+
+    private getFilterFunction(): FilterFunction {
+        try {
+            this.clearErrorMessage()
+            return eval("(email) => " + this.functionBodyTextArea.value)
+        } catch (e) {
+            this.setErrorMessage(e.message)
+        }
+    }
+
+    private createElements() {
+        this.presetMenu = newElm("select", {}, 
             Object.entries(this.presets).map(([value, [name]]) => 
                 newElm("option", {value: value}, [text(name)]))
         )
-        this.textArea = newElm("textArea", {class: "filter-function-body-input", spellcheck:"false"}, []) as HTMLTextAreaElement
+        this.functionBodyTextArea = newElm("textArea", {class: "filter-function-body-input", spellcheck:"false"}, []) as HTMLTextAreaElement
 
-        this.text = newElm("code", {class:"language-javascript"}, [])
+        this.highlightedFunctionBodyText = newElm("code", {class:"language-javascript"}, [])
 
         this.errorElm = div({class: "filter-function-error"}, [])
 
-        const functionSig = newElm("code", {class: "language-javascript, filter-function-sig"}, [text("(email) => ")])
+        this.functionSignature = newElm("code", {class: "language-javascript, filter-function-sig"}, [text("(email) => ")])
 
 
-        this.textWrapper = div({class: "filter-function-wrapper"}, [
-            functionSig,
+        this.functionWrapper = div({class: "filter-function-wrapper"}, [
+            this.functionSignature,
 
             div({class: "filter-function-body-wrapper"}, [
-                newElm("pre", {class: "filter-highlighted-text"}, [this.text]),
-                this.textArea,
+                newElm("pre", {class: "filter-highlighted-text"}, [this.highlightedFunctionBodyText]),
+                this.functionBodyTextArea,
             ]),
             this.errorElm
         ])
@@ -63,15 +104,17 @@ export class FilterOptions {
             div({class: "filter-left-right-split"}, [
                 div({class: "filter-left-panel"}, [
                     newElm("label", {}, [text("Presets")]),
-                    this.menu,
+                    this.presetMenu,
                 ]),
-                this.textWrapper,
+                this.functionWrapper,
                 div({class:"filter-button-container"}, [this.applyButton])
             ])
         ], this.container)
+    }
 
-        selectorObserable(this.menu).subscribe(option => {
-            this.textArea.value = this.presets[option][1]
+    private setupEvents() {
+        selectorObserable(this.presetMenu).subscribe(option => {
+            this.functionBodyTextArea.value = this.presets[option][1]
             this.updateText()
         })
 
@@ -82,54 +125,21 @@ export class FilterOptions {
             })    
         }).pipe(share())
 
-        this.noError()
-
-        this.textArea.addEventListener("input", () => {
+        this.functionBodyTextArea.addEventListener("input", () => {
             this.updateText()
         })
-        this.updateText()
-        highlightElement(functionSig)
     }
 
     private updateText() {
-        this.text.textContent = this.textArea.value    
-        highlightElement(this.text)
+        this.highlightedFunctionBodyText.textContent = this.functionBodyTextArea.value    
+        highlightElement(this.highlightedFunctionBodyText)
     }
 
-    private getFilterFunction(): FilterFunction {
-        try {
-            this.noError()
-            return eval("(email) => " + this.textArea.value)
-        } catch (e) {
-            // this.errorElm.textContent = e.message
-            this.error(e.message)
-        }
-    }
-
-    filterEmails(emails: Observable<Email[]>): Observable<Email[]> {
-        return new Observable<Email[]>(sub => {
-            combineLatest([
-                emails,
-                this.filterFunction
-            ]).subscribe(([emails, filterFunc]) => {
-                const emailMap = arrayToObject(emails, email => email.id)
-                const people = getCorrespondants(emails)
-    
-                try {
-                    sub.next(emails.filter(email => filterFunc(email, people, emailMap)))
-                    this.noError()
-                } catch (e) {
-                    this.error(e.message)
-                }
-            })
-        }).pipe(share())
-    }
-
-    error(msg: string) {
+    private setErrorMessage(msg: string) {
         this.errorElm.style.display = "block"
         this.errorElm.textContent = msg
     }
-    noError() {
+    private clearErrorMessage() {
         this.errorElm.style.display = "none"
     }
 }
